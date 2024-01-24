1615abc370fc58096eea90fa9d7c5bf38eaa91f5
Uyi Aguebor <uyiaguebor@gmail.com>
2024-01-24 19:51:10 +0000

    removed api folder

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,1 +1,9 @@
+import axios from "axios";
 
+export default axios.create({
+  baseURL: "https://api.yelp.com/v3/businesses",
+  headers: {
+    Authorization:
+      "Bearer Fj1Hq095PzYy5smGMcakV__XrKDLjbLXaU8i9v3fDBHluUcf3y51VLoXfaMvgzU6ca-W3b2NhEfj_8FG1f3m1uZ0847MoFWwZq8pmQnDwTnJbhdmQaZ3sglZWwSXZXYx",
+  },
+});
